2be14d7b62a459f473a1cf68443de0bcda633b66
benediktebenedikte <34661737+benediktebenedikte@users.noreply.github.com>
2017-12-19 16:57:02 +0100

    I did some changes

--- gamejam/src/Assets/Components/sections/games.js
+++ gamejam/src/Assets/Components/sections/games.js
@@ -6,7 +6,7 @@ class Games extends Component {
     return (
         <div className="container-fluid">
         <div class="row games">
-          <div class="col-md-5 offset-md-1 gamelist">
+          <div class="col-md-6 offset-md-2 gamelist">
                 <h1 class="title">Games</h1>
                 <div class="row">
                  <div class="col-sm-6 game"><img src= {logo2} class="game1" width="108,42" height="93,71" /></div>
@@ -25,7 +25,7 @@ class Games extends Component {
                    <div class="col-sm-6 game"><img src= {logo2} class="game11" width="108,42" height="93,71" /></div>
                 </div>
             </div>
-            <div class="col-md-6 gameDetails"></div>
+            <div class="col-md-4 gameDetails"></div>
 
         </div>
         </div>
